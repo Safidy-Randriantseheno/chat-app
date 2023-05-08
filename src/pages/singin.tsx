33ebf47b2bf270f2c2865d3281253a87bd530f30
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-05-08 17:58:51 +0300

    feat: add singin

---
+++ src/pages/singin.tsx
@@ -0,0 +1,77 @@
+import { useRouter } from "next/router"
+import { useState } from "react"
+
+const Signin = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const router = useRouter()
+    return(
+        <div className= 'css1'>
+            <form className='css2'>
+                <div>
+                    <h4>Sign in</h4>
+                </div>
+                <div className='flex flex-col mb-1'>
+                    <label>Username: </label>
+                    <input className='us1'
+                        type='text'
+                        value={username}
+                        onChange={
+                            (e) => {
+                                setUsername(e.target.value)
+                            }
+                        }
+                    />
+                </div>
+                <div className='us1'>
+                    <label>Email: </label>
+                    <input className='us1' 
+                        type='email'
+                    />
+                </div>
+                <div className='flex flex-col mb-1'>
+                    <label>Password: </label>
+                    <input className='us1' 
+                        type='password'
+                        value={password}
+                        onChange={
+                            (e) => {
+                                setPassword(e.target.value)
+                            }
+                        }
+                    />
+                </div>
+                <div className='us1'>
+                    <label>Confirm password: </label>
+                    <input className='us1'
+                        type='password'
+                        value={confirmPassword}
+                        onChange={
+                            (e) => {
+                                setConfirmPassword(e.target.value)
+                            }
+                        }
+                    />
+                </div>
+                <div className='bouton'
+                    onClick={
+                        () => {
+                            if(password === confirmPassword){
+                                localStorage.setItem(username as string, password as string)
+                                localStorage.setItem("currentUser",username as string)
+                                router.push('/chatpublic')
+                            } else {
+                                alert("Veillez verifier votre mot de passe")
+                            }
+                        }
+                    }
+                >
+                    <p>Continue</p> 
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Signin

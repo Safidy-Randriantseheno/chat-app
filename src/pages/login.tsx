00ac625515e6fb3d68f606bcfa2eb5f0e52ca678
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-05-08 17:58:09 +0300

    feat: add login

---
+++ src/pages/login.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  return (
+    <div className='log1'>
+      <form className='log2'>
+        <div className="log">
+          <label>
+            Username:
+            <input 
+              type='text'
+              value={username}
+              onChange={(e) => {setUsername(e.target.value)}}
+            />
+          </label>
+        </div>
+        <div className='log'>
+          <label>
+            PassWord:
+            <input 
+              type="password"
+              value={password}
+              onChange={(e) => {setPassword(e.target.value)}}
+            />
+          </label>
+        </div>
+        <div className='bouton1'  
+        onClick={
+            () => {
+                if(localStorage.getItem(username as string) === password){
+                    localStorage.setItem("currentUser",username as string)
+                    router.push('/chatpublic')
+                } else {
+                        alert('user not found')
+                        }
+                    }
+                }>
+          <button>
+            se connecter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
